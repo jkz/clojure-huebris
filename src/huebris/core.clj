19b37d8d984aa8b34a2d9cee453869a26a2f76b1
Jesse the Game <jesse@jessethegame.net>
2015-01-21 01:52:33 +0000

    Tidy up a little

--- src/huebris/core.clj
+++ src/huebris/core.clj
@@ -14,10 +14,7 @@
 
 (declare sunset-lights-task)
 
-(defstruct units :temp :distance :pressure :speed)
-(defstruct wind :chill :speed)
 (defstruct astronomy :sunrise :sunset)
-(defstruct condition :text :temp :date)
 
 (def yahoourl "http://weather.yahooapis.com/forecastrss?u=c&w=")
 (def huerl (env :hue-url))
@@ -30,43 +27,12 @@
 
 (defn get-weather [woeid]
   (let [zipper (get-weather-feed woeid)
-        z-location (zf/xml1-> zipper :channel :yweather:location)
-        z-units (zf/xml1-> zipper :channel :yweather:units)
-        z-wind (zf/xml1-> zipper :channel :yweather:wind)
-        z-astronomy (zf/xml1-> zipper :channel :yweather:astronomy)
-        z-condition (zf/xml1-> zipper :channel :item :yweather:condition)]
+        z-astronomy (zf/xml1-> zipper :channel :yweather:astronomy)]
     {
-    :city (zf/attr z-location :city)
-    :country (zf/attr z-location :country)
-    :units (struct-map units
-                       :temp (zf/attr z-units :temperature)
-                       :distance (zf/attr z-units :distance)
-                       :pressure (zf/attr z-units :pressure)
-                       :speed (zf/attr z-units :speed))
-    :wind (struct-map wind
-                       :chill (zf/attr z-wind :chill)
-                       :speed (zf/attr z-wind :speed))
     :astronomy (struct-map astronomy
                        :sunrise (zf/attr z-astronomy :sunrise)
                        :sunset (zf/attr z-astronomy :sunset))
-    :condition (struct-map condition
-                       :temp (zf/attr z-condition :temp)
-                       :text (zf/attr z-condition :text)
-                       :date (zf/attr z-condition :date))
      }))
-
-(defn print-weather [info]
-  (let [condition (:condition info)
-        units (:units info)]
-  (println "Weather at" (:date condition) ","
-           (:city info) "," (:country info))
-  (println (condition :text) ","
-           (condition :temp)
-           (units :temp))))
-
-(defn print-weather-london
-  []
-  (print-weather (get-weather woeid-london)))
 
 (defn hue-groups-state
   [group, params]
@@ -78,26 +44,6 @@
        :content-type :json
        :throw-entire-message? true})
     (println group params)))
-
-(defn turn-it-off-and-on-again
-  []
-  (do
-    (hue-groups-state 0 {:on false})
-    (at-at/after 2000 #(hue-groups-state 0 {:on true :hue (rand-int 65535) :sat 255 :transitiontime 1}) task-pool)))
-
-(defn slow-pulse
-  []
-  (do
-    (hue-groups-state 0 {:bri 127 :transitiontime 50})
-    (at-at/after 10000 #(hue-groups-state 0 {:bri 255 :hue (rand-int 65535) :transitiontime 50}) task-pool)))
-
-(defn bow-chicka-wow-wow []
-  (do
-    (hue-groups-state 0 {:on true :sat 255})
-    (at-at/every 20000 #(slow-pulse) task-pool :desc "Sensually pulsating hues")))
-
-(defn party []
-  (at-at/every 4000 #(turn-it-off-and-on-again) task-pool :desc "Flicker lights"))
 
 (defn stop []
   (at-at/stop-and-reset-pool! task-pool))
